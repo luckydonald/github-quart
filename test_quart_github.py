7cae2a8ca77f461a9352f96aad64fdfe8e4523da
luckydonald <mac2019.git@luckydonald.de>
2023-06-29 18:42:21 +0200

    quart-rewrite: adapted the test.

--- test_quart_github.py
+++ test_quart_github.py
@@ -1,32 +1,37 @@
+import asyncio
 import logging
 import unittest
-
-import requests
+import httpx
 from mock import patch, Mock
 
-from flask import Flask, request, redirect
-from flask_github import GitHub
+from quart import Quart, request, redirect
+from quart_github import GitHub
 
 logger = logging.getLogger(__name__)
 
 
-class GitHubTestCase(unittest.TestCase):
+class GitHubTestCase(unittest.IsolatedAsyncioTestCase):
 
-    @patch.object(requests.Session, 'post')
+    async def asyncSetUp(self) -> None:
+        self.loop = asyncio.get_running_loop()
+    # end if
+
+    @patch.object(httpx.AsyncClient, 'post')
     @patch.object(GitHub, 'BASE_AUTH_URL')
-    def test_authorization(self, auth_url, post):
+    async def test_authorization(self, auth_url, post):
         def assert_params(*args, **kwargs):
             data = kwargs.pop('data')
-            assert data['client_id'] == '123'
-            assert data['client_secret'] == 'SEKRET'
-            assert data['code'] == 'KODE'
+            logger.info(f'assert_params(*{args!r}, **{kwargs}')
+            self.assertEqual(data['client_id'], '123', "data['client_id'] == '123'")
+            self.assertEqual(data['client_secret'], 'SEKRET', "data['client_secret'] == 'SEKRET'")
+            self.assertEqual(data['code'], 'KODE', "data['code'] == 'KODE'")
             response = Mock()
             response.content = b'access_token=asdf&token_type=bearer'
             return response
         post.side_effect = assert_params
         auth_url.__get__ = Mock(return_value='http://localhost/oauth/')
 
-        app = Flask(__name__)
+        app = Quart(__name__)
 
         app.config['GITHUB_CLIENT_ID'] = '123'
         app.config['GITHUB_CLIENT_SECRET'] = 'SEKRET'
@@ -34,35 +39,41 @@ class GitHubTestCase(unittest.TestCase):
         github = GitHub(app)
 
         @app.route('/login')
-        def login():
+        async def login():
+            logger.info(f'login()')
             return github.authorize(redirect_uri="http://localhost/callback")
 
         @app.route('/callback')
         @github.authorized_handler
-        def authorized(token):
+        async def authorized(token):
+            logger.info(f'authorized(token={token!r})')
             access_token.append(token)
             return ''
 
         # Mimics GitHub authorization URL
         # http://developer.github.com/v3/oauth/#web-application-flow
         @app.route('/oauth/authorize')
-        def handle_auth():
+        async def handle_auth():
+            logger.info(f'handle_auth()')
             logger.info("in /oauth/authorize")
             called_auth.append(1)
-            assert request.args['client_id'] == '123'
-            logger.debug("client_id OK")
-            assert request.args['redirect_uri'] == 'http://localhost/callback'
-            logger.debug("redirect_uri OK")
+            self.assertEqual(request.args['client_id'], '123', "request.args['client_id'] == '123'")
+            logger.info("client_id OK")
+            self.assertEqual(request.args['redirect_uri'], 'http://localhost/callback', "request.args['redirect_uri'] == 'http://localhost/callback'")
+            logger.info("redirect_uri OK")
             return redirect(request.args['redirect_uri'] + '?code=KODE')
 
         access_token = []
         called_auth = []
 
         client = app.test_client()
-        client.get('/login', follow_redirects=True)
+        logger.info('CALLING /login')
+        await client.get('/login', follow_redirects=True)
+        logger.info('CALLED /login')
 
-        assert called_auth
-        assert access_token == ['asdf'], access_token
+        self.assertTrue(called_auth)
+        self.assertTrue(access_token)
+        self.assertEqual(access_token, ['asdf'], "access_token == ['asdf']")
 
 
 if __name__ == '__main__':
